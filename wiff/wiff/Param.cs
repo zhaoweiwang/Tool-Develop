db566fa5cde78d63fc290bd4aa440bb82971a72a
Mako <zhaowwang@gmail.com>
2015-09-14 14:20:53 +0800

    init

---
+++ wiff/wiff/Param.cs
@@ -0,0 +1,209 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Collections;
+using System.IO;
+
+using System.Windows.Forms.DataVisualization.Charting;
+using System.Windows.Forms;
+
+namespace wiff
+{
+    class Param
+    {
+        public List<string> inputfiles = new List<string>();
+        public int ms1 = 0;
+        public int ms2 = 0;
+        public int mgf = 1;
+        public bool cent = true;//默认中心化
+        public string outputPath = "";
+        //默认导出mgf
+        //默认进行中心化 p为FALSE c为TRUE outputpath为空
+
+        public void getAllInputFile(ref List<string> list)
+        {
+            list = this.inputfiles;
+        }
+
+        public void listAllFiles(ref List<string> inFiles)
+        {
+            string error1 = "[wiff] <Exception>: Failed to find file ";
+            string error2 = "[wiff] <Exception>: Warning: No wiff files.";
+            for (int i = 0; i < inFiles.Count(); i++)
+            {
+                //判断为单个文件还是路径
+                if (!Directory.Exists(inFiles[i]) && !File.Exists(inFiles[i]))
+                {
+                    //既不是文件也不是路径，退出
+                    Console.WriteLine("{0} {1}", error1, inFiles[i]);
+                    Environment.Exit(0);
+                    //Application.Exit();
+                }
+                else if (File.Exists(inFiles[i]))
+                {
+                    //是文件，检查是否以 wiff结尾
+                    FileInfo fi = new FileInfo(inFiles[i]);
+                    if (fi.Extension.ToUpper().CompareTo(".WIFF") == 0)
+                    {
+                        inputfiles.Add(fi.ToString());//文件名都以.wiff结尾
+                    }
+                }
+                else if (Directory.Exists(inFiles[i]))
+                {
+                    //是路径，检查路径中是否有 wiff结尾
+                    bool haswiff = false;
+                    DirectoryInfo folder = new DirectoryInfo(inFiles[i]);
+                    foreach (FileInfo tmpfi in folder.GetFiles())
+                    {
+                        if (tmpfi.Extension.ToUpper().CompareTo(".WIFF") == 0)
+                        {
+                            haswiff = true;
+                            string filepath = inFiles[i] + "\\" + tmpfi;
+                            inputfiles.Add(filepath);
+                        }
+                    }
+                    if (haswiff == false)
+                    {
+                        Console.WriteLine("[wiff] <Exception>: Find no wiffs in "+inFiles[i]+"!");
+                    }
+                }
+            }
+            if (inputfiles.Count == 0)
+            {
+                Console.WriteLine(error2);
+                Environment.Exit(0);
+                // Application.Exit();
+            }
+        }
+
+        public void checkOutputPath()
+        {
+            //输出路径 文件夹
+            if (this.outputPath.CompareTo("") == 0)
+            {
+                return;
+            }
+            else
+            {
+                if (!Directory.Exists(this.outputPath))
+                {
+                    //路径不存在 构建路径
+                    try
+                    {
+                        Directory.CreateDirectory(this.outputPath);
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine("[wiff] <Exception>: The process failed {0}", e.ToString());
+                    }
+                }
+            }
+        }
+
+        public void showHelpInformation()
+        {
+            Console.WriteLine("NAME");
+            Console.WriteLine("\twiff.exe - extract text-plain spectra\n");
+            Console.WriteLine("SYNOPSIS");
+            Console.WriteLine("\twiff.exe [OPTION]... [FILE]...\n");
+            Console.WriteLine("DESCRIPTION");
+            Console.WriteLine("\tExtract text-plain spectra from WIFF FILES.\n");
+            Console.WriteLine("\t--help");
+            Console.WriteLine("\t\tGet help information.\n");
+            Console.WriteLine("\t-mgf");
+            Console.WriteLine("\t\tExport mgf files.\n");
+            Console.WriteLine("\t-ms");
+            Console.WriteLine("\t\tExport MS1 and MS2 files.\n");
+            Console.WriteLine("\t-ms1");
+            Console.WriteLine("\t\tExport MS1 files.\n");
+            Console.WriteLine("\t-ms2");
+            Console.WriteLine("\t\tExport MS2 files.\n");
+            Console.WriteLine("\t-o");
+            Console.WriteLine("\t\tOutputs directory. e.g. -o F:\\data\\output\n");
+            Console.WriteLine("\t-c");
+            Console.WriteLine("\t\tMake results centroided. In default case, this switch is on.\n");
+            Console.WriteLine("\t-p");
+            Console.WriteLine("\t\tMake results profile. In default case, this switch is off.\n");
+        }
+
+        public void ParseParam(string[] args)
+        {
+            List<string> tmpinput = new List<string>();
+            this.mgf = 0;
+            //args[0] 不是 XXX.exe
+            string error = "Usage: Invoke.exe input_file_path ... [-options]\nPlease refer to the user guide for more details.\n";
+            try
+            {
+                if (args.Length == 0)
+                {
+                    showHelpInformation();
+                    return;
+                }
+                for (int i = 0; i < args.Length; i++)
+                {
+                    if (args[i][0] == '-')
+                    {
+                        if (args[i].CompareTo("-ms1") == 0)
+                        {
+                            this.ms1 = 1;
+                        }
+                        else if (args[i].CompareTo("-ms2") == 0)
+                        {
+                            this.ms2 = 1;
+                        }
+                        else if (args[i].CompareTo("-ms") == 0)
+                        {
+                            this.ms1 = 1;
+                            this.ms2 = 1;
+                        }
+                        else if (args[i].CompareTo("-mgf") == 0)
+                        {
+                            this.mgf = 1;
+                        }
+                        else if (args[i].CompareTo("-p") == 0)
+                        {
+                            this.cent = false;//profile 不用中心化
+                        }
+                        else if (args[i].CompareTo("-c") == 0)
+                        {
+                            this.cent = true;
+                        }
+                        else if (args[i].CompareTo("-o") == 0)
+                        {
+                            i++;
+                            this.outputPath = args[i];
+                        }
+                        else if (args[i].CompareTo("--help") == 0)
+                        {
+                            showHelpInformation();
+                            Environment.Exit(0);
+                            //Application.Exit();
+                        }
+                        else
+                        {
+                            throw new Exception(error);
+                        }
+                    }
+                    else
+                    {
+                        tmpinput.Add(args[i].ToString());
+                        //要处理的原始文件 路径和文件两种写法
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+                Environment.Exit(0);
+                //Application.Exit();
+                //命令行异常  退出程序
+            }
+            if (this.ms1 == 0 && this.ms2 == 0 && this.mgf == 0)
+                this.mgf = 1;
+            checkOutputPath();
+            listAllFiles(ref tmpinput);
+        }
+    }
+}
---
+++ wiff/wiff/Program.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Collections;
+using System.IO;
+using System.Diagnostics;
+using System.Windows.Forms.DataVisualization.Charting;
+using System.Windows.Forms;
+using Clearcore2.RawXYProcessing;
+using Clearcore2.Data;
+using Clearcore2.Data.AnalystDataProvider;
+using Clearcore2.Data.DataAccess;
+using Clearcore2.Data.DataAccess.SampleData;
+using Clearcore2.ProjectUtilities;
+using Clearcore2.StructuredStorage;
+using Clearcore2.Utility;
+using Clearcore2.ProjectUtilities.Options;
+
+namespace wiff
+{
+    class Program
+    {
+        public static void Parse(Spectrum spec, WiffFile wifffile,int cycleCount, int experimentCount, string ms1file,string ms2file,string mgffile,Param para)
+        {
+            StreamWriter writerms1 = null; 
+            StreamWriter writerms2 = null; 
+            StreamWriter writermgf = null; 
+            if (para.ms1 == 1)
+            {
+                writerms1 = new StreamWriter(new FileStream(ms1file, FileMode.Create, FileAccess.Write));
+                spec.WriteHeader(writerms1,para);
+            }
+            if (para.ms2 == 1)
+            {
+                writerms2 = new StreamWriter(new FileStream(ms2file, FileMode.Create, FileAccess.Write));
+                spec.WriteHeader(writerms2, para);
+            }
+
+            if (para.mgf == 1)
+            {
+                writermgf = new StreamWriter(new FileStream(mgffile, FileMode.Create, FileAccess.Write));
+            }
+
+            for (int j = 0; j < cycleCount; j++)
+            {
+                string header = "[wiff] <Output files>: ";
+                Console.Write("\r" + header + String.Format("{0:F3}", (double)(j + 1) / (double)cycleCount * 100) + "%");
+                for (int i = 0; i < experimentCount; i++)
+                {
+                    TotalIonChromatogram totalIonChromatogram = spec.array[i];
+                    float num = (float)totalIonChromatogram.GetYValue(j);
+                    if (i == 0 || num > 0f)
+                    {
+                        bool doCentroid = false;
+                        spec.cycle = j + 1;
+                        spec.experiment = i + 1;
+                        spec.m_msExperiment = spec.wiffExperiments[i];
+                        spec.m_spectrum = spec.m_msExperiment.GetMassSpectrum(j);
+                        spec.details = spec.m_msExperiment.Details;
+                        spec.m_spectrumInfo = spec.m_msExperiment.GetMassSpectrumInfo(j);
+                        spec.pointsAreContinuous = !spec.m_spectrumInfo.CentroidMode;
+
+                        if (spec.GetDataSize(doCentroid) > 0)
+                        {
+
+                            List<double> centermzs = new List<double>();
+                            List<double> centerint = new List<double>();
+                            spec.GetData(doCentroid, out centermzs, out centerint);
+
+                            if (para.cent == true)
+                            {
+                                PeakDetection.WindowCentroid(centermzs, centerint, out spec.mz, out spec.intensity,spec);
+                            }
+                            else
+                            {
+                                spec.mz = centermzs;
+                                spec.intensity = centerint;
+                            }
+
+                            spec.retentionTime = (double)totalIonChromatogram.GetXValue(j);
+                            spec.precursor_mz = 0.0;
+                            spec.charge = 0;
+                            if (spec.m_spectrumInfo.IsProductSpectrum)
+                            {
+                                spec.precursor_mz = spec.m_spectrumInfo.ParentMZ;
+                                spec.charge = spec.m_spectrumInfo.ParentChargeState;
+                            }
+                            spec.scan++;
+                            //scan从1开始
+                            if (spec.GetMSLevel() == 1 && para.ms1 == 1)
+                            {
+                                spec.WriteMS1(writerms1);
+                            }else if(spec.GetMSLevel() > 1){
+                                if (para.ms2 == 1)
+                                {
+                                    spec.WriteMS2(writerms2);
+                                }
+                                if (para.mgf == 1)
+                                {
+                                   
+                                    spec.WriteMGF(wifffile,writermgf);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            if (para.ms1 == 1)
+            {
+                writerms1.Close();
+            }
+            if (para.ms2 == 1)
+            {
+                writerms2.Close();
+            }
+            if (para.mgf == 1)
+            {
+                writermgf.Close();
+            }
+
+            Console.WriteLine();
+        }
+
+        public static void extract(string wiffpath, Param para)
+        {
+            WiffFile wifffile = WiffFile.Create(wiffpath);
+            int pos = wiffpath.LastIndexOf(".");
+            if (para.outputPath == "")
+            {
+                para.outputPath = wiffpath.Substring(0, pos);
+            }
+            else
+            {
+                para.outputPath = para.outputPath + "\\" + wifffile.name;
+            }
+
+            Console.WriteLine("{0} {1}", "[wiff] Begin to extract ", wiffpath);
+            Spectrum spec = new Spectrum();
+
+            string mgffile = para.outputPath + ".mgf";
+            string ms1file = para.outputPath + ".ms1";
+            string ms2file = para.outputPath + ".ms2";
+            for (int sample = 1; sample <= wifffile.m_batch.GetSampleNames().Length; ++sample)
+            {
+                wifffile.GetSample(sample);
+                //Experiment数目
+                int experimentCount = wifffile.m_sample.MassSpectrometerSample.ExperimentCount;
+                spec.array = new TotalIonChromatogram[experimentCount];
+                spec.scan = 0;
+                for (int i = 0; i < experimentCount; i++)
+                {
+                    spec.m_msExperiment = wifffile.m_sample.MassSpectrometerSample.GetMSExperiment(i);
+                    spec.wiffExperiments.Add(spec.m_msExperiment);
+                    spec.array[i] = spec.m_msExperiment.GetTotalIonChromatogram();
+                }
+                int numDataPoints = spec.array[0].NumDataPoints;
+                //Cycle数目
+                Parse(spec,wifffile,numDataPoints,experimentCount,ms1file,ms2file,mgffile,para);
+
+            }
+            Console.WriteLine("[wiff] Output Files Completed!");
+        }
+
+
+        [STAThread]
+        static void Main(string[] args)
+        {
+            Application.EnableVisualStyles();
+            Stopwatch watch = new Stopwatch();
+            watch.Start();
+            List<string> files = new List<string>();
+            Param para = new Param();
+            para.ParseParam(args);
+            para.getAllInputFile(ref files);
+            if (files.Count == 0 && args.Length != 0)
+            {
+                Console.WriteLine("[wiff] <Exception>: Warning: No input files.");
+                return;
+            }
+            else
+            {
+                for (int i = 0; i < files.Count; i++)
+                {
+                    extract(files[i], para);
+                }
+            }
+            watch.Stop();
+            string runtime = (watch.ElapsedMilliseconds / 1000).ToString();
+            Console.WriteLine("{0} {1}", "[wiff] == == == Time elapsed: ", runtime + "s. == == ==");
+            return;
+        }
+	}
+
+}
